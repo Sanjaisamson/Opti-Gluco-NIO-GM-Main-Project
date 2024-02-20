6464b91cabc6f2423e556f03cd48fedc1dc28a91
9400390752 <sanjaisamson3225@gmail.com>
2024-02-20 18:46:54 +0530

    updated second commit

--- Iot_server/src/services/clientServices.js
+++ Iot_server/src/services/clientServices.js
@@ -24,23 +24,23 @@ async function createJob(payload) {
 async function readData(job_id, job_status) {
   try {
     let jobStatus = job_status;
-    // const imageCaptured = exec(
-    //   "raspistill -o image%d.jpg -tl 8000 -t 128000",
-    //   (error, stdout, stderr) => {
-    //     if (error) {
-    //       jobStatus = "failed";
-    //       console.error(`Error capturing images: ${error.message}`);
-    //       throw new Error(500, "Internal server error !!!");
-    //     }
-    //     if (stderr) {
-    //       jobStatus = "failed";
-    //       console.error(`Error capturing images: ${stderr}`);
-    //       throw new Error(500, "Internal server error !!!");
-    //     }
-    //     console.log("Images captured successfully...");
-    //     jobStatus = "on-progress";
-    //   }
-    // );
+    const imageCaptured = exec(
+      "raspistill -o image%d.jpg -tl 8000 -t 128000",
+      (error, stdout, stderr) => {
+        if (error) {
+          jobStatus = "failed";
+          console.error(`Error capturing images: ${error.message}`);
+          throw new Error(500, "Internal server error !!!");
+        }
+        if (stderr) {
+          jobStatus = "failed";
+          console.error(`Error capturing images: ${stderr}`);
+          throw new Error(500, "Internal server error !!!");
+        }
+        console.log("Images captured successfully...");
+        jobStatus = "on-progress";
+      }
+    );
     const images = [];
     for (let i = 1; i <= 15; i++) {
       const filename =
--- MainServer/src/routes/productRoutes.js
+++ MainServer/src/routes/productRoutes.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const productRouter = express.Router()
-const productController = require('../controllers/productController')
-const authHandler = require('../middlewares/authHandler.middleware')
+const express = require("express");
+const productRouter = express.Router();
+const productController = require("../controllers/productController");
+const authHandler = require("../middlewares/authHandler.middleware");
 
-
-productRouter.post('/register',authHandler.accessTokenVerification,productController.registerProduct)
-productRouter.post('/remove',authHandler.accessTokenVerification,productController.removeProduct)
-productRouter.post('/read-data',authHandler.accessTokenVerification,productController.readData)
+productRouter.post(
+  "/register",
+  authHandler.accessTokenVerification,
+  productController.registerProduct
+);
+productRouter.post(
+  "/remove",
+  authHandler.accessTokenVerification,
+  productController.removeProduct
+);
+productRouter.post(
+  "/read-data",
+  authHandler.accessTokenVerification,
+  productController.readData
+);
 
 module.exports = productRouter;
